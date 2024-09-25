cb5c69a14363975424032fb3c1b4ec3539eacab5
Bee Brain <beebrain@Bees-MacBook-Pro.local>
2024-09-25 20:33:03 +0100

    body parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express= require("express");
 const mongoose= require("mongoose");
 require('dotenv').config();
 const cors= require("cors")
+const bodyParser= require("body-parser");
 
 const app= express();
 app.use(express.json());
@@ -9,6 +10,7 @@ const port= process.env.port;
 const mongo= process.env.mongoUri
 const Router=require("./route/termRoute")
 app.use(cors());
+app.use(bodyParser.json())
 mongoose.connect(mongo).then(()=>{
     console.log(`DB Connected`);
     app.listen(port, ()=>{
